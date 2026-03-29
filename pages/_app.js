38b8b3a711eeee54e394a6cbbb7035daccb42365
researchrains-ux <researchrains@gmail.com>
2026-03-30 00:03:46 +0200

    light theme redesign

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,10 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>The Leftover Wardrobe</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Curated pre-loved and new clothing. Pickup in Nürnberg." />
+        <meta name="description" content="Curated pre-loved and new clothing. Pickup or delivery in Schweinfurt." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
